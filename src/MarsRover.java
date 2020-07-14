4c11ffe456542c5c8feaf7d504e072dfb3145aea
Abhay2608 <35658612+Abhay2608@users.noreply.github.com>
2020-07-14 18:59:37 +0530

    Added createMarsRover test method
    Check whether MarsRover object is getting created

--- src/MarsRover.java
+++ src/MarsRover.java
@@ -1,2 +1,28 @@
+import java.util.Objects;
+
 public class MarsRover {
+    private final int x;
+    private final int y;
+    private final char direction;
+
+    public MarsRover(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MarsRover marsRover = (MarsRover) o;
+        return x == marsRover.x &&
+                y == marsRover.y &&
+                direction == marsRover.direction;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, direction);
+    }
 }
--- test/MarsRoverTest.java
+++ test/MarsRoverTest.java
@@ -1,12 +1,19 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 public class MarsRoverTest {
+
     @Test
     public void testPositive(){
         assertEquals(true,true);
     }
 
-    
+    @Test
+    public void createMarsRover(){
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        assertThat(marsRover,is(new MarsRover(0,0,'N')));
+    }
 }
