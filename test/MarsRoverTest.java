67d40e7b4daea56d82cf35e4fb306c8cad4aeabe
Abhay2608 <35658612+Abhay2608@users.noreply.github.com>
2020-07-14 18:31:51 +0530

    Added MarsRoverTest class
    Added MarsRoverTest class
    added testPositive() method

--- test/MarsRoverTest.java
+++ test/MarsRoverTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MarsRoverTest {
+    @Test
+    public void testPositive(){
+        assertEquals(true,true);
+    }
+
+    
 }
